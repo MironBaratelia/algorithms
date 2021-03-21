7a4a8e12dba6e511d473af6ed7a6e3690bb32497
MironBaratelia <52035086+MironBaratelia@users.noreply.github.com>
2021-03-21 18:45:02 +0300

    Create factorize.cpp

---
+++ factorize.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> factorize(int x) {
+	vector<int> ans;
+	for (int i = 2; i * i <= x; ++i) {
+		while (x % i == 0) {
+			ans.push_back(i);
+			x /= i;
+		}
+	}
+	if (x != 1) {
+		ans.push_back(x);
+	}
+	return ans;
+}
+
+int main() {
+
+}
