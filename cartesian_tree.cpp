a2709dfa68e450cb9df31d37fc484199a56606ca
MironBaratelia <52035086+MironBaratelia@users.noreply.github.com>
2021-06-20 20:21:21 +0300

    Update cartesian_tree.cpp

--- cartesian_tree.cpp
+++ cartesian_tree.cpp
@@ -55,28 +55,84 @@ PNode merge(PNode l, PNode r) {
         return l;
     }
     else {
-        r->l = merge(r->l, l);
+        r->l = merge(l, r->l);
         update(r);
         return r;
+    }
+}
+
+void del(PNode& node, int x) {
+    if (!node) return;
+    if (node->key == x) {
+        node = merge(node->l, node->r);
+    }
+    else if (node->key > x) {
+        del(node->l, x);
+        update(node);
+    }
+    else {
+        del(node->r, x);
+        update(node);
+    }
+}
+
+int exists(PNode node, int x) {
+    if (!node) return 0;
+    if (node->key == x) {
+        return 1 + getW(node->l);
+    }
+    else if (node->key > x) {
+        return exists(node->l, x);
+    }
+    else {
+        int w = exists(node->r, x);
+        return w ? w + getW(node->l) + 1 : w;
+    }
+}
+
+PNode insert(PNode node, int x) {
+    if (exists(node, x)) {
+        return node;
+    }
+    auto tmp1 = split(node, x);
+    auto tmp2 = merge(tmp1.first, new Node(x));
+    return merge(tmp2, tmp1.second);
+}
+
+void print_tree(PNode node) {
+    if (node) {
+        print_tree(node->l);
+        cout << node->key << "(" << node->w << ") ";
+        print_tree(node->r);
     }
 }
 
 int sum(PNode root, int l, int r) {
     auto tmp1 = split(root, l);
     auto tmp2 = split(tmp1.second, r + 1);
-    int ans = getSum(tmp2.first);
-    tmp1.second = merge(tmp2.first, tmp2.second);
-    root = merge(tmp1.first, tmp1.second);
-    return ans;
+    return getSum(tmp2.first);
 }
 
 int main() {
     gen.seed((int)time(0));
 
     PNode root = nullptr;
-    for (int i = 0; i <= 10; ++i) {
-        PNode tmp = new Node(i);
-        root = merge(root, tmp);
+    
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        string s;
+        int x;
+        cin >> s >> x;
+
+        if (s[0] == 'i') {
+            root = insert(root, x);
+        }
+        else if (s[0] == 'e') {
+            cout << (exists(root, x) ? "true" : "false") << '\n';
+        }
+        else if (s[0] == 'd') {
+            del(root, x);
+        }
     }
-    cout << sum(root, 0, 9) << '\n';
 }
